ada46779114224a91bf38618db99c602058bd21d
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-07 17:23:43 -0300

    feat(nix): add dotnet-sdk 9 to flake shell declaration

--- flake.nix
+++ flake.nix
@@ -5,35 +5,37 @@
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      allSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs allSystems (
-          system:
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (
+        system:
           f {
-            pkgs = import nixpkgs { inherit system; };
+            pkgs = import nixpkgs {inherit system;};
           }
-        );
-    in
-    {
-      devShells = forAllSystems (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              dotnetCorePackages.sdk_8_0_3xx
-            ];
-          };
-        }
       );
-    };
+  in {
+    devShells = forAllSystems (
+      {pkgs}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (with dotnetCorePackages;
+              combinePackages [
+                sdk_9_0
+                sdk_8_0
+              ])
+          ];
+        };
+      }
+    );
+  };
 }
