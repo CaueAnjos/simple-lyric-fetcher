ea3e1ae448aa0185a063933675faa2048a23e832
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-16 21:59:56 -0300

    feat(nix): add flake.nix for devshell

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs
+
+  outputs = {self, ...} @ inputs: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+      );
+  in {
+    devShells = forEachSupportedSystem (
+      {pkgs}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [];
+
+          env = {};
+
+          shellHook = "";
+        };
+      }
+    );
+  };
+}
