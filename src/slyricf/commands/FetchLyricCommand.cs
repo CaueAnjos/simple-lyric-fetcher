472d9956598f52c71cbcd1444a6415c99daa04dc
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-11-28 19:21:07 -0300

    feat: add fetch command settings

--- src/slyricf/commands/FetchLyricCommand.cs
+++ src/slyricf/commands/FetchLyricCommand.cs
@@ -5,6 +5,12 @@ namespace slyricf.commands;
 
 class FetchLyricCommand : Command
 {
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<url>")]
+        public string Url { get; set; } = string.Empty;
+    }
+
     public override int Execute(CommandContext context)
     {
         AnsiConsole.MarkupLine("Hello, [cyan]World![/]");
